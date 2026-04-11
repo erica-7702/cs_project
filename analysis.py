a099beaed9ebbe6cac5a122f302c3307d192ba6a
erica-7702 <erica.boakye@ashesi.edu.gh>
2026-04-11 15:15:07 +0000

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -1,20 +1,24 @@
 def spending_analysis(transactions):
-    total_expense=0
-    category_totals={}
+    total_expense = 0
+    category_totals = {}
     for t in transactions:
-                if t[0] == "Expense":
-                    amount = t[1]
-                    category = t[2]
+        if t[0] == "Expense":
+            amount = t[1]
+            category = t[2]
 
-                    total_expense += amount
+            total_expense += amount
 
-                    if category not in category_totals:
-                        category_totals[category] = 0
+            if category not in category_totals:
+                category_totals[category] = 0
 
-                    category_totals[category] += amount
+            category_totals[category] += amount
 
     print("\n~ Spending Analysis ~")
     print("Total Expenses:", total_expense)
+
+    print("\nSpending by Category:")
+    for cat in category_totals:
+        print(cat, ":", category_totals[cat])
 
     highest_category = ""
     highest_amount = 0
@@ -26,6 +30,20 @@ def spending_analysis(transactions):
 
     if highest_category != "":
         print("Highest Spending Category:", highest_category, "-", highest_amount)
+
+        print("\nAverage Spending per Category:")
+        for cat in category_totals:
+            count = 0
+            total = 0
+
+            for t in transactions:
+                if t[0] == "Expense" and t[2] == cat:
+                    total += t[1]
+                    count += 1
+
+            if count > 0:
+                average = total / count
+                print(cat, "average spending:", average)
 
     else:
         print("No expenses recorded")
